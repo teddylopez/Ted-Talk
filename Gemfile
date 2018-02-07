0aa89ab3472dc14550446e9ef064aedc918dec6c
teddylopez <tedmlopez@gmail.com>
2018-02-06 21:49:46 -0500

    Building out About-Me page.

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 ruby '2.2.5'
 
 gem 'rails', '~> 5.1.4'
+gem 'jquery-rails'
 gem 'puma', '~> 3.7'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,16 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing');
+	});
+});
