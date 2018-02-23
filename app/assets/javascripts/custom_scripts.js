f1a16a3169154942eb6a0a0ea1882b98fb6eedd0
teddylopez <tedmlopez@gmail.com>
2018-02-23 18:38:06 -0500

    Troubleshooting PROD events.

--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -10,7 +10,7 @@ var tedmetrics = {};
 tedmetrics.report = function(eventName){
   var event = {event: { name: eventName }};
   var request = new XMLHttpRequest();
-  request.open("POST", "https://tedmetrics.herokuapp.com/api/events", true);
+  request.open("POST", "http://tedmetrics.herokuapp.com/api/events", true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send(JSON.stringify(event));
 };
