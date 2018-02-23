e06138b1832720d797b0ed8887fc3f46d889d6f2
teddylopez <tedmlopez@gmail.com>
2018-02-23 17:24:00 -0500

    Adding tedmetrics tracking.

--- app/assets/javascripts/custom_scripts.js
+++ app/assets/javascripts/custom_scripts.js
@@ -6,17 +6,11 @@ $(document).ready(function(){
   }, 2500);
 });
 
-var blocmetrics = {};
-
-blocmetrics.report = function(eventName){
-  // #1
+var tedmetrics = {};
+tedmetrics.report = function(eventName){
   var event = {event: { name: eventName }};
-  // #2
   var request = new XMLHttpRequest();
-  // #3
   request.open("POST", "http://localhost:3000/api/events", true);
-  // #4
   request.setRequestHeader('Content-Type', 'application/json');
-  // #5
   request.send(JSON.stringify(event));
 };
