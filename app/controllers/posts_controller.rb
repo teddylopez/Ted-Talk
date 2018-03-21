9e78273e19fb47ce340b7b0312de71263f45377c
teddylopez <tedmlopez@gmail.com>
2018-03-21 09:35:25 -0400

    Edited pagination.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
   before_action :find_post, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-  
+
   def index
-    @posts = Post.all.order("created_at DESC").paginate(page: params[:page], per_page: 10)
+    @posts = Post.all.order("created_at DESC").paginate(page: params[:page], per_page: 5)
   end
 
   def new
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
   def index
     @posts = Post.all.limit(4).order("created_at DESC")
-    @projects = Project.all.limit(4).order("created_at DESC")
+    @projects = Project.all.limit(5).order("created_at DESC")
   end
 
   def about
