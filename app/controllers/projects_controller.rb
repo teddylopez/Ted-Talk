ea74f1fc9baa16130209f854b78a584804871502
teddylopez <tedmlopez@gmail.com>
2018-03-21 09:54:18 -0400

    Styled pagination.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -3,7 +3,7 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
 
   def index
-    @projects = Project.all.order("created_at desc")
+    @projects = Project.all.order("created_at DESC").paginate(page: params[:page], per_page: 5)
   end
 
   def new
